d27c0c4ea2953f3edde5a7b23fc492e402f1cfb3
AshishNehe <ashishnehe1@gmail.com>
2026-09-01 05:54:17 +0530

    Add first code in JShell

---
+++ 2.3 First Code in Jshell.java
@@ -0,0 +1,15 @@
+|  Welcome to JShell -- Version 17.0.3.1
+
+jshell> System.out.println("Hello World")
+Hello World
+
+jshell> System.out.println("Navin Reddy, Telusko")
+Navin Reddy, Telusko
+
+jshell> 2+4
+$3 ==> 6
+
+jshell> 9-6
+$4 ==> 3
+
+jshell>
